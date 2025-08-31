098e07676e6ecbd284b3ba11d05bbe357aaff956
Hatice Aparci <haticeaparci@users.noreply.github.com>
2025-08-31 19:52:42 +0200

    Cors tested

--- api/orders.js
+++ api/orders.js
@@ -1,8 +1,3 @@
-import fs from "fs/promises";
-import path from "path";
-
-const ordersFile = path.join(process.cwd(), "api", "data", "orders.json");
-
 export default async function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Access-Control-Allow-Methods", "GET,POST,OPTIONS");
@@ -18,11 +13,11 @@ export default async function handler(req, res) {
     if (!orderData || !orderData.items || orderData.items.length === 0) {
       return res.status(400).json({ message: "Missing data." });
     }
-    // Dosya işlemlerini kaldırdık
+
     const newOrder = { ...orderData, id: (Math.random() * 1000).toString() };
     res.status(201).json({ message: "Order created!", order: newOrder });
   } else if (req.method === "GET") {
-    res.status(200).json([]); // Her zaman boş dizi döner
+    res.status(200).json([]);
   } else {
     res.status(405).json({ message: "Method not allowed" });
   }
